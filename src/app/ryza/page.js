c86497fc87074ba7f279952ed4451bd001291cfd
reyyymartt <reymartcenteno03@gmail.com>
2025-05-19 00:33:28 +0800

    update

--- src/app/ryza/page.js
+++ src/app/ryza/page.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function About() {
 	return (
-		<div className="m-2 card bg-body-tertiary">
+		<div className="m-2 card dyna">
 			<h1 className="card-header">hi</h1>
 			<p className="card-body">I could spend lifetimes trying to name the exact shade your soul burns in, and still fall short</p>
 		</div>
