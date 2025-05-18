627afc8b1130fb38eae26218178330c4fa8c20d0
reyyymartt <reymartcenteno03@gmail.com>
2025-05-18 23:58:06 +0800

    update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -9,7 +9,6 @@ export default function Home() {
       <header className="bg-blue-600 py-16 text-center">
         <h1 className="text-4xl font-bold">Ryza maliit</h1>
         <p className="mt-4">BWHAHAHAHAHAHAHAHAHHAAHAHHAHAHAHAHAHA</p>
-        <p>Username: {user}</p>
         <button className="btn btn-primary mt-6 font-semibold rounded-lg">Learn More</button>
       </header>
     </div>
