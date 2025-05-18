3d2944fc8db6805e9253952d0b6ea6e39ff9b9fa
reyyymartt <reymartcenteno03@gmail.com>
2025-05-19 00:17:30 +0800

    update

---
+++ src/app/ryza/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+
+export default function About() {
+	return (
+		<div className="m-2 card">
+			<h1 className="card-header">???</h1>
+			<p className="card-body">wht r we</p>
+		</div>
+	)
+}
