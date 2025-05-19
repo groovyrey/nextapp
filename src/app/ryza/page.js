ec4c5196e2b5871d2fd74a5cd2bee2035e4f3b2d
reyyymartt <reymartcenteno03@gmail.com>
2025-05-19 08:30:44 +0800

    update page.js

--- src/app/ryza/page.js
+++ src/app/ryza/page.js
@@ -4,7 +4,7 @@ export default function About() {
 	return (
 		<div className="m-2 card dyna">
 			<h1 className="card-header">hi</h1>
-			<p className="card-body">I could spend lifetimes trying to name the exact shade your soul burns in, and still fall short</p>
+			<p className="card-body">{btoa("I could spend lifetimes trying to name the exact shade your soul burns in, and still fall short")}</p>
 		</div>
 	)
 }
