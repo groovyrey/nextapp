55853e0ab687483ebee603ac76f5c40640a6403c
reyyymartt <reymartcenteno03@gmail.com>
2025-05-20 20:11:47 +0800

    update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const user = process.env.NEXT_PUBLIC_USERNAME
+const user = process.env.NEXT_PUBLIC_API_URL
 
 export default function Home() {
   return (
@@ -9,6 +9,7 @@ export default function Home() {
       <header className="bg-blue-600 py-16 text-center">
         <h1 className="text-4xl font-bold">Ryza maliit</h1>
         <p className="mt-4">BWHAHAHAHAHAHAHAHAHHAAHAHHAHAHAHAHAHA</p>
+        <p>Data: {user}</p>
         <button className="btn btn-primary mt-6 font-semibold rounded-lg">Learn More</button>
       </header>
     </div>
