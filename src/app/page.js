26990dc3e5d65e7b68a3e005261d4746ba4839b6
reyyymartt <reymartcenteno03@gmail.com>
2025-05-22 20:48:31 +0800

    update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -5,8 +5,8 @@ export default function Home() {
     <div className="min-h-screen flex flex-col">
       {/* Hero Section */}
       <header className="bg-blue-600 py-16 text-center">
-        <h1 className="text-4xl font-bold">Welcome to Reymart's site</h1>
-        <p className="mt-4">Title</p>
+        <h1 className="text-4xl font-bold">Test webpage</h1>
+        <p className="mt-4">test description</p>
         <button className="btn btn-primary mt-6 font-semibold rounded-lg">Learn More</button>
       </header>
     </div>
