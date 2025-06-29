d2291621364971126f41ee84e250894ee197c2eb
reyyymartt <reymartcenteno03@gmail.com>
2025-06-29 15:26:08 +0800

    update

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -52,11 +52,13 @@ export default function LoginPage() {
 
 	return (
 		<div className="container mt-5">
-      <h2>Login1</h2>
+		  <div className="card-body">
+      <h2 className="card-title">Login1</h2>
       {error && <p className="text-danger">{error}</p>}
       <input type="email" className="form-control my-2" placeholder="Email" onChange={e => setEmail(e.target.value)} />
       <input type="password" className="form-control my-2" placeholder="Password" onChange={e => setPassword(e.target.value)} />
       <button className="btn btn-primary" onClick={handleLogin}>Login</button>
+      </div>
     </div>
 	);
 }
--- src/app/page.js
+++ src/app/page.js
@@ -42,12 +42,6 @@ export default function Home() {
     <div className="min-h-screen flex flex-col">
       <UserDisplay />
       {logoutError && <p className="text-danger">{logoutError}</p>}
-      {/* Hero Section */}
-      <header className="bg-blue-600 py-16 text-center">
-        <h1 className="text-4xl font-bold">Test webpage</h1>
-        <p className="mt-4">test description</p>
-        <button className="btn btn-primary mt-6 font-semibold rounded-lg">Learn More</button>
-      </header>
     </div>
   );
 }
