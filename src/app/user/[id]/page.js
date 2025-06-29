a7d921ac19841b9a816e0fa2c891dd88ae76d3d8
reyyymartt <reymartcenteno03@gmail.com>
2025-06-30 05:46:49 +0800

    update page.js

--- src/app/user/[id]/page.js
+++ src/app/user/[id]/page.js
@@ -44,7 +44,7 @@ export default function UserProfilePage({ params }) {
     return <div className="text-danger">Error: {error}</div>;
   }
 
-  if (!profileData && !loading) {
+  if (!profileData) {
     return <div>No profile data found.</div>;
   }
 
