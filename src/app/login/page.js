057bf566297ff7a831dbbe8044bd433ab3bddc36
reyyymartt <reymartcenteno03@gmail.com>
2025-06-29 15:30:39 +0800

    update page.js

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -52,12 +52,14 @@ export default function LoginPage() {
 
 	return (
 		<div className="container mt-5">
-		  <div className="card-body">
-      <h2 className="card-title">Login1</h2>
+		  <div className="card">
+		    <div className="card-body">
+      <h2 className="card-title">Login</h2>
       {error && <p className="text-danger">{error}</p>}
       <input type="email" className="form-control my-2" placeholder="Email" onChange={e => setEmail(e.target.value)} />
       <input type="password" className="form-control my-2" placeholder="Password" onChange={e => setPassword(e.target.value)} />
       <button className="btn btn-primary" onClick={handleLogin}>Login</button>
+      </div>
       </div>
     </div>
 	);
