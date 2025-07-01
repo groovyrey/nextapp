1e9ecadde37837ace37eeca9730c07cecede7152
groovyrey <reymartcenteno03@gmail.com>
2025-07-01 20:19:59 +0800

    page transitions

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,8 @@ import { UserProvider } from "./context/UserContext";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import Navbar from "./components/Navbar";
+import { AnimatePresence, motion } from "framer-motion";
+import { usePathname } from "next/navigation";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,12 +15,25 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+  const pathname = usePathname();
+
   return (
     <html lang="en">
       <body className={inter.className}>
         <UserProvider>
           <Navbar />
-          <main className="container py-5">{children}</main>
+          <AnimatePresence mode="wait">
+            <motion.main
+              key={pathname}
+              className="container py-5"
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: -20 }}
+              transition={{ duration: 0.3 }}
+            >
+              {children}
+            </motion.main>
+          </AnimatePresence>
         </UserProvider>
       </body>
     </html>
