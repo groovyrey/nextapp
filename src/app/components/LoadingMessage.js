d00ca361171f441a19c18548305dc9de7782bd01
groovyrey <reymartcenteno03@gmail.com>
2025-07-02 03:34:27 +0800

    loading

--- src/app/components/LoadingMessage.js
+++ src/app/components/LoadingMessage.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import Lottie from 'react-lottie';
+import animationData from '../../../public/loading_animation.json'; // Adjust path as needed
 import styles from './LoadingMessage.module.css';
 
 const LoadingMessage = () => {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice'
+    }
+  };
+
   return (
     <div className={`${styles.loadingMessageContainer} d-flex justify-content-center align-items-center`} style={{ height: '100vh' }}>
       <div className="text-center">
-        <div className="spinner-border text-primary" role="status">
-          <span className="visually-hidden">Loading...</span>
-        </div>
+        <Lottie options={defaultOptions} height={200} width={200} />
         <p className="mt-3">Loading...</p>
       </div>
     </div>
