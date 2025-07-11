fed46a8afc9526e5ed7de0269e4af6f329d16da3
groovyrey <reymartcenteno03@gmail.com>
2025-07-11 15:03:18 +0800

    feat(auth): Update Lead Developer icon and color

--- src/app/utils/AuthRankSystem.js
+++ src/app/utils/AuthRankSystem.js
@@ -1,5 +1,5 @@
 const AUTH_LEVEL_RANKS = {
-    1: { title: "Lead Developer", icon: "bi-boxes", color: "text-danger" },
+    1: { title: "Lead Developer", icon: "bi-terminal", color: "text-success" },
     2: { title: "Developer", icon: "bi-code-slash", color: "text-info" },
     // Add more auth levels as needed
 };
